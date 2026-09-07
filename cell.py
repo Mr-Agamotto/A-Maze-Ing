5aebb82d3c19416ec79e3c310af85dce4e8973bc
bat-sudo <martim.oliveirak@gmail.com>
2026-09-07 14:47:42 +0100

    Add files via upload

---
+++ cell.py
@@ -0,0 +1,8 @@
+class Cell:
+   def __init__(self) -> None:
+        self.coords = {
+                "North" = True,
+                "East" = True,
+                "West" = True,
+                "South" = True
+                }
