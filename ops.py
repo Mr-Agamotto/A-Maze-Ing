5ab19813c17da0624f4cac000e7aa899e8f7f2b3
bat-sudo <martim.oliveirak@gmail.com>
2026-09-07 13:07:15 +0100

    Add files via upload

---
+++ ops.py
@@ -0,0 +1,11 @@
+print("=== A-Maze-ing ===")
+print("1. Re-generate a new maze")
+print("2. Show / Hide all the shortest path")
+print("3. Rotate the wall colours")
+print("4. Quit")
+option = input("Choice? (1-4):")
+if option != "1" or "2" or "3" or "4":
+    print("Invalid option.")
+    print("
+    
+
