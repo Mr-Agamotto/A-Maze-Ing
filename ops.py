19e706d55f44e233ca7cd8e24b8df963b9b4599f
bat-sudo <martim.oliveirak@gmail.com>
2026-09-09 18:09:43 +0100

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,10 +1,14 @@
 import os
 
-
-def menu() -> str:
+def menu():
     print("=== A-Maze-ing ===")
     print("1. Re-generate a new maze")
     print("2. Show / Hide all the shortest path")
-    print("3. Rotate the wall colours")
+   print("3. Rotate the wall colours")
     print("4. Quit")
-    return input("Choice? (1-4): ").strip()
+    option = input("Choice? (1-4): ")
+    if option == "1":
+      os.system("clear")
+      os.system("python3 a_maze_ing.py config.txt")
+    if option == "4":
+        os.system("clear")
